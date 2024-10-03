9b0e473da61907845099b5c4fc0d51f07928f3c7
SheilandraZarawiba29 <Sheilandrazarawiba561@gmail.com>
2024-10-03 08:21:37 +0700

    Pertanyaan 1 percobaan 1

--- Pemilihan2Percobaan128.java
+++ Pemilihan2Percobaan128.java
@@ -11,6 +11,8 @@ public class Pemilihan2Percobaan128 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
                 System.out.println("Tahun kabisat");
+            else 
+                System.out.println("Bukan tahun kabisat");
         } else {
             System.out.println("Bukan tahun kabisat");
         }
